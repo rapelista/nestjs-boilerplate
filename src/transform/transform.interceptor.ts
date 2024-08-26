f57b2a1c3fcbffa6131f69e50d2477a4e23cda08
Farih Akmal Haqiqi <akmal@gamatecha.com>
2024-08-26 21:45:40 +0700

    fix: update transformers

--- src/transform/transform.interceptor.ts
+++ src/transform/transform.interceptor.ts
@@ -7,47 +7,9 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-export interface Response {
-  data: any;
-  meta?: {
-    page: number;
-    total_data: number;
-    total_page: number;
-  };
-}
-
 @Injectable()
-export class TransformInterceptor<T> implements NestInterceptor<T, Response> {
-  intercept(
-    context: ExecutionContext,
-    next: CallHandler,
-  ): Observable<Response> {
-    return next.handle().pipe(
-      map((data) => this.responseHandler(data, context)),
-      // catchError((err: HttpException) =>
-      //   throwError(() => this.errorHandler(err, context)),
-      // ),
-    );
-  }
-
-  // errorHandler(exception: unknown, context: ExecutionContext) {
-  //   const ctx = context.switchToHttp();
-  //   const response = ctx.getResponse();
-
-  //   if (exception instanceof HttpException) {
-  //   } else if (exception instanceof Prisma.PrismaClientKnownRequestError) {
-  //     response.status(500).json({
-  //       error: 'dari prisma',
-  //     });
-  //   } else if (exception instanceof ZodError) {
-  //   }
-
-  //   response.status(500).json({
-  //     timestamp: new Date().toISOString(),
-  //   });
-  // }
-
-  responseHandler(res: any, _: ExecutionContext) {
-    return { ...res };
+export class TransformInterceptor<T> implements NestInterceptor<T> {
+  intercept(_: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(map((data) => ({ ...data })));
   }
 }
