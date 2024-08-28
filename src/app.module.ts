f4397b5d13766335b8ea06287890ca0e6d502271
Farih Akmal Haqiqi <akmal@gamatecha.com>
2024-08-28 20:17:20 +0700

    feat(home): add app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 import { PrismaModule } from './databases/prisma/prisma.module';
 import { UsersModule } from './models/users/users.module';
 
 @Module({
   imports: [UsersModule, PrismaModule],
+  controllers: [AppController],
 })
 export class AppModule {}
